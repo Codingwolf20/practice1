f7b3d118613009b918768e53a79751096d2266d9
Sadegh saber <161755425+Codingwolf20@users.noreply.github.com>
2024-03-12 19:23:28 +0330

    Create Gridbutton.py
    Grid button 9 number 2d

---
+++ Gridbutton.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+
+ROWS = COLS = 3
+
+class GridApp(App):
+    def build(self):
+        root = GridLayout(rows=ROWS, cols=COLS)
+        for i in range(ROWS):
+            for j in range(COLS):
+                root.add_widget(Button(text=f"({i}, {j})"))
+        return root
+
+GridApp().run()
